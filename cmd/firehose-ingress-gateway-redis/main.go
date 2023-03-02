fe0699fec086ce020731377e2f5be8e36960523b
berquerant <sin@sin-pro.local>
2023-03-03 01:50:11 +0900

    Extract common startup process

--- cmd/firehose-ingress-gateway-redis/main.go
+++ cmd/firehose-ingress-gateway-redis/main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/signal"
 
 	"github.com/berquerant/firehose-ingress-gateway/pkg/redis"
+	"github.com/berquerant/firehose-ingress-gateway/pkg/runner"
 	"github.com/berquerant/firehose-ingress-gateway/server"
-	"github.com/caarlos0/env/v7"
 	goredis "github.com/redis/go-redis/v9"
-	"go.uber.org/zap"
 )
 
 type config struct {
@@ -27,51 +24,27 @@ type config struct {
 }
 
 func main() {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
+	var cfg config
 
-	var (
-		cfg  config
-		opts = env.Options{
-			OnSet: func(tag string, value any, isDefault bool) {
-				logger.Info("Read env",
-					zap.String("name", tag),
-					zap.Any("value", value),
-					zap.Bool("default", isDefault),
-				)
-			},
-		}
-	)
-
-	if err := env.Parse(&cfg, opts); err != nil {
-		logger.Fatal("Failed to read env",
-			zap.Error(err),
-		)
-	}
-	if err := run(&cfg); err != nil {
-		logger.Fatal("Run",
-			zap.Error(err),
-		)
+	if err := runner.New(cfg).Run(context.Background(), run); err != nil {
+		panic(err)
 	}
 }
 
-func run(cfg *config) error {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
+func run(ctx context.Context, cfg config) error {
 	rdb := goredis.NewClient(&goredis.Options{
 		Addr:     cfg.RedisAddr,
 		Password: cfg.RedisPass,
 		DB:       cfg.RedisDB,
 	})
 	if _, err := rdb.Ping(ctx).Result(); err != nil {
-		return fmt.Errorf("Failed to connect to redis: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
 	messenger := redis.New(rdb, cfg.RedisQueueKey)
 	srv := server.New(messenger)
 	if err := server.Run(ctx, srv, cfg.Port); err != nil {
-		return fmt.Errorf("Run: %w", err)
+		return fmt.Errorf("run: %w", err)
 	}
 	return nil
 }
